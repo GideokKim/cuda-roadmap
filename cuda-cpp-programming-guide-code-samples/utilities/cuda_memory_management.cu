26c7ab5d922f0bcffe7fedc7635e245420dd3e10
gideok Kim <ikid4127@naver.com>
2025-03-09 09:43:08 +0900

    ⚡️ Impl template functions for cuda memory management

--- cuda-cpp-programming-guide-code-samples/utilities/cuda_memory_management.cu
+++ cuda-cpp-programming-guide-code-samples/utilities/cuda_memory_management.cu
@@ -2,30 +2,47 @@
 
 #include "cuda_memory_management.cuh"
 
-void allocateDeviceMemory(int **d_data, int size) {
-  cudaError_t err = cudaMalloc(d_data, size * sizeof(int));
+template <typename T>
+void allocateDeviceMemory(T **d_data, int size) {
+  cudaError_t err = cudaMalloc(d_data, size * sizeof(T));
   if (err != cudaSuccess) {
     std::cerr << "Error allocating device memory: " << cudaGetErrorString(err)
               << std::endl;
   }
 }
 
-void freeDeviceMemory(int *d_data) { cudaFree(d_data); }
+template <typename T>
+void freeDeviceMemory(T *d_data) {
+  cudaFree(d_data);
+}
 
-void copyHostToDevice(int *h_data, int *d_data, int size) {
+template <typename T>
+void copyHostToDevice(T *h_data, T *d_data, int size) {
   cudaError_t err =
-      cudaMemcpy(d_data, h_data, size * sizeof(int), cudaMemcpyHostToDevice);
+      cudaMemcpy(d_data, h_data, size * sizeof(T), cudaMemcpyHostToDevice);
   if (err != cudaSuccess) {
     std::cerr << "Error copying from host to device: "
               << cudaGetErrorString(err) << std::endl;
   }
 }
 
-void copyDeviceToHost(int *d_data, int *h_data, int size) {
+template <typename T>
+void copyDeviceToHost(T *d_data, T *h_data, int size) {
   cudaError_t err =
-      cudaMemcpy(h_data, d_data, size * sizeof(int), cudaMemcpyDeviceToHost);
+      cudaMemcpy(h_data, d_data, size * sizeof(T), cudaMemcpyDeviceToHost);
   if (err != cudaSuccess) {
     std::cerr << "Error copying from device to host: "
               << cudaGetErrorString(err) << std::endl;
   }
 }
+
+// 템플릿 인스턴스화
+template void allocateDeviceMemory<int>(int **d_data, int size);
+template void freeDeviceMemory<int>(int *d_data);
+template void copyHostToDevice<int>(int *h_data, int *d_data, int size);
+template void copyDeviceToHost<int>(int *d_data, int *h_data, int size);
+
+template void allocateDeviceMemory<float>(float **d_data, int size);
+template void freeDeviceMemory<float>(float *d_data);
+template void copyHostToDevice<float>(float *h_data, float *d_data, int size);
+template void copyDeviceToHost<float>(float *d_data, float *h_data, int size);
--- cuda-cpp-programming-guide-code-samples/utilities/cuda_memory_management.cuh
+++ cuda-cpp-programming-guide-code-samples/utilities/cuda_memory_management.cuh
@@ -4,15 +4,19 @@
 #include <cuda_runtime.h>
 
 // 디바이스 메모리 할당 함수
-void allocateDeviceMemory(int **d_data, int size);
+template <typename T>
+void allocateDeviceMemory(T **d_data, int size);
 
 // 디바이스 메모리 해제 함수
-void freeDeviceMemory(int *d_data);
+template <typename T>
+void freeDeviceMemory(T *d_data);
 
 // 호스트 메모리에서 디바이스 메모리로 데이터 복사
-void copyHostToDevice(int *h_data, int *d_data, int size);
+template <typename T>
+void copyHostToDevice(T *h_data, T *d_data, int size);
 
 // 디바이스 메모리에서 호스트 메모리로 데이터 복사
-void copyDeviceToHost(int *d_data, int *h_data, int size);
+template <typename T>
+void copyDeviceToHost(T *d_data, T *h_data, int size);
 
 #endif  // CUDA_MEMORY_MANAGEMENT_H
